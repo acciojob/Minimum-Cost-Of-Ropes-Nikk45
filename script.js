29ee6939ea1c478f42e288e895ec3f614297c6a9
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-16 15:36:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+   const element = document.getElementByID("rope-lengths");
+	
+	const str = element.target.value;
+	const arr = str.split(',');
+	alert(arr);
 }  
