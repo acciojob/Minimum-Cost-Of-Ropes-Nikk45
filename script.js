cf67b2a1691f4bf58a495e9f96f0c59e0b31f870
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-16 16:30:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 function calculateMinCost() {
   //your code here
-	
+  // let arr = [1, 3, 20, 5];
+
+// arr.sort((a,b)=>a-b);
+
+// let res = [];
+
+// while(arr.length > 1){
+    
+//     let val1 = arr.shift();
+//     let val2 = arr.shift();
+
+//     let sum = val1 + val2;
+    
+//     arr.push(sum);
+//     res.push(sum);
+//     arr.sort((a,b)=> a-b);
+// }
+
+// let sum = 0;
+// for(let x of res){
+//     sum+=x;
+// }
+
+// console.log(sum);
+  
+  
 }  
-
-
-
-// code for getting minimum number
-
-let arr = [1, 3, 20, 5];
-
-arr.sort((a,b)=>a-b);
-
-let res = [];
-
-while(arr.length > 1){
-    
-    let val1 = arr.shift();
-    let val2 = arr.shift();
-
-    let sum = val1 + val2;
-    
-    arr.push(sum);
-    res.push(sum);
-    arr.sort((a,b)=> a-b);
-}
-
-let sum = 0;
-for(let x of res){
-    sum+=x;
-}
-
-console.log(sum);
