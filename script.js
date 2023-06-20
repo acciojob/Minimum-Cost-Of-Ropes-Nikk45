844f9f596fbec2f1bb1dae8273e2954757d67d5f
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-20 15:53:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 function calculateMinCost() {
   //your code here
-  // let arr = [1, 3, 20, 5];
+	const input = document.getElementsByTagName('input')[0];
+  
+	let inputValues = input.value.split(',');
+	
+  // let arr = [4, 2, 7, 6, 9];
+  let arr =[];
+  for(let x of inputValues){
+    let num = parseInt(x);
+    arr.push(num);
+  }
+  
+arr.sort((a,b)=>a-b);
+  // alert(arr);
 
-// arr.sort((a,b)=>a-b);
+let res = [];
 
-// let res = [];
-
-// while(arr.length > 1){
+while(arr.length > 1){
     
-//     let val1 = arr.shift();
-//     let val2 = arr.shift();
+    let val1 = arr.shift();
+    let val2 = arr.shift();
 
-//     let sum = val1 + val2;
+    let sum = val1 + val2;
     
-//     arr.push(sum);
-//     res.push(sum);
-//     arr.sort((a,b)=> a-b);
-// }
+    arr.push(sum);
+    res.push(sum);
+    arr.sort((a,b)=> a-b);
+}
 
-// let sum = 0;
-// for(let x of res){
-//     sum+=x;
-// }
+let sum = 0;
+for(let x of res){
+    sum+=x;
+  // console.log(typeof(x));
+}
 
 // console.log(sum);
-  
-  
+	const result = document.getElementById('result');
+	result.innerText = sum;
+
 }  
+
+
